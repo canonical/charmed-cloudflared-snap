72eadc09be2a94c0d06bcbebf531511f8f107b01
Weii Wang <weii.wang@canonical.com>
2025-06-27 17:20:58 +0800

    Fix

--- snap/local/entrypoint.py
+++ snap/local/entrypoint.py
@@ -22,6 +22,23 @@ def get_metrics_port() -> int | None:
     return int(port) if port else None
 
 
+def get_proxy_env() -> dict:
+    http_proxy = get_config("http-proxy")
+    https_proxy = get_config("https-proxy")
+    no_proxy = get_config("no-proxy")
+    env = {}
+    if http_proxy:
+        env["HTTP_PROXY"] = http_proxy
+        env["http_proxy"] = http_proxy
+    if https_proxy:
+        env["HTTPS_PROXY"] = https_proxy
+        env["https_proxy"] = https_proxy
+    if no_proxy:
+        env["NO_PROXY"] = no_proxy
+        env["no_proxy"] = no_proxy
+    return env
+
+
 def main():
     os.setgroups([])
     os.setregid(584792, 584792)
@@ -41,6 +58,7 @@ def main():
                 "run",
             ],
             env={
+                **get_proxy_env(),
                 "TUNNEL_TOKEN": token,
             },
         )
